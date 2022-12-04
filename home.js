5ab73f7bd4bd6c1eb17bd1d7fcbcf459aef5cd08
supriya-05 <93468102+supriya-05@users.noreply.github.com>
2022-12-04 13:53:36 +0530

    Added javascript file of home page

---
+++ home.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+  $(function() {
+    
+    $("h2")
+        .wrapInner("<span>")
+
+    $("h2 br")
+        .before("<span class='spacer'>")
+        .after("<span class='spacer'>");
+
+});
